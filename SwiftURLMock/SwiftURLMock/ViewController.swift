4f4aedfbade7808affde65028d0ce1ad88ce36d4
Mike Chirico <mchirico@gmail.com>
2019-02-15 16:09:30 -0500

    Initial commit

---
+++ SwiftURLMock/SwiftURLMock/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SwiftURLMock
+//
+//  Created by Michael Chirico on 2/15/19.
+//  Copyright © 2019 Michael Chirico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+}
